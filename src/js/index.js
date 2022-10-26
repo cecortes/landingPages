e5f5c9f19e26b9841acb01d08fd10e4c18d3b838
César López Cortés <cesarlopezcortes@hotmail.com>
2022-10-25 22:57:54 -0500

    Footer done and animations

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,19 @@
 "use strict";
 
 //* Ready *//
-$(function () {});
+$(function () {
+  const inViewport = (entries, observer) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+    });
+  };
+
+  const Obs = new IntersectionObserver(inViewport);
+  const obsOptions = {}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+
+  // Attach observer to every [data-inviewport] element:
+  const ELs_inViewport = document.querySelectorAll("[element-inView]");
+  ELs_inViewport.forEach((EL) => {
+    Obs.observe(EL, obsOptions);
+  });
+});
